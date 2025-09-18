6f278c24e7b8152dcc30b2f803166f46d66f8b2a
Joy Chesire <chesirejoy39@gmail.com>
2025-09-18 20:15:51 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+console.log(dodger);
+
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left = "0px";
+
+/* document.addEventListener("keydown", function (event) {
+  console.log(event);
+}); */
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
